861f4ad3682b0b73bc6f894129bc70cf6f05dd26
陈斌 <1786512286@qq.com>
2024-08-19 15:58:39 +0800

    新增:完成了线段编辑和渲染3d的工作可使用了/three-example/#/Corners

--- src/pages/Corners/index.jsx
+++ src/pages/Corners/index.jsx
@@ -1,5 +1,5 @@
 import * as THREE from "three";
-import { createElement, useRef, useEffect } from "react";
+import { createElement, useRef, useEffect ,useState} from "react";
 import { World } from "@/utils/three.utils";
 
 import TWEEN from "three/addons/libs/tween.module.js";
@@ -9,10 +9,11 @@ import { OutputPass } from "three/addons/postprocessing/OutputPass.js";
 import { RenderPass } from "three/addons/postprocessing/RenderPass.js";
 import { ShaderPass } from "three/addons/postprocessing/ShaderPass.js";
 import Edit2DLine from "./Edit2DLine";
+
+let scene, camera, renderer;
 export default function () {
 	const canvasRef = useRef(null);
-	
-	let scene, camera, renderer;
+	const [list, setList] = useState([])
 	useEffect(() => {
 		const canvas = canvasRef.current;
 		const destroyList=[]
@@ -24,8 +25,7 @@ export default function () {
 			world.addOrbitControls();
 
 			({ scene, camera, renderer } = world);
-			camera.position.set(0,0,1000);
-
+			camera.position.set(0,0,-2000);
 			const ambientLight = new THREE.AmbientLight(0x404040, 10); // 柔和的白光
 			world.createModel(ambientLight);
 
@@ -37,63 +37,167 @@ export default function () {
 			// scene.add(mesh);
 		}
 		
-	const list=[
-		{
-			"start": {
-				"x": 450,
-				"y": 400
+		const list=[
+			{
+				"start": {
+					"x": 600,
+					"y": 350
+				},
+				"end": {
+					"x": 600,
+					"y": 650
+				}
 			},
-			"end": {
-				"x": 450,
-				"y": 550
-			}
-		},
-		{
-			"start": {
-				"x": 450,
-				"y": 550
+			{
+				"start": {
+					"x": 600,
+					"y": 650
+				},
+				"end": {
+					"x": 1100,
+					"y": 650
+				}
 			},
-			"end": {
-				"x": 750,
-				"y": 550
+			{
+				"start": {
+					"x": 1100,
+					"y": 650
+				},
+				"end": {
+					"x": 1100,
+					"y": 350
+				}
+			},
+			{
+				"start": {
+					"x": 1100,
+					"y": 350
+				},
+				"end": {
+					"x": 900,
+					"y": 350
+				}
+			},
+			{
+				"start": {
+					"x": 900,
+					"y": 350
+				},
+				"end": {
+					"x": 900,
+					"y": 250
+				}
+			},
+			{
+				"start": {
+					"x": 900,
+					"y": 250
+				},
+				"end": {
+					"x": 800,
+					"y": 250
+				}
+			},
+			{
+				"start": {
+					"x": 800,
+					"y": 250
+				},
+				"end": {
+					"x": 800,
+					"y": 350
+				}
+			},
+			{
+				"start": {
+					"x": 800,
+					"y": 350
+				},
+				"end": {
+					"x": 600,
+					"y": 350
+				}
+			},
+			{
+				"start": {
+					"x": 0,
+					"y": 300
+				},
+				"end": {
+					"x": 0,
+					"y": 750
+				}
+			},
+			{
+				"start": {
+					"x": 600,
+					"y": 0
+				},
+				"end": {
+					"x": 1100,
+					"y": 0
+				}
 			}
+		]
+		let ratio=5
+		
+		return ()=>{
+			destroyList.forEach(fun=>fun())
 		}
-	]
-	let ratio=50
+	}, []);
+	//清理scene
+	scene?.children.forEach((item)=>{
+		if(item.type==="Mesh"){
+			scene.remove(item)
+		}
+	})
 	list.forEach(({start,end})=>{
 		const heartShape = new THREE.Shape();
-		
-		heartShape.moveTo(start.x,start.y);
-		heartShape.lineTo(end.x,end.y);
+		let x=window.innerWidth/2,y=window.innerHeight/2
+		heartShape.moveTo(-start.x+x,-start.y+y);
+		heartShape.lineTo(-end.x+x,-end.y+y);
 
 		const extrudeSettings = { depth: 100, bevelEnabled: true, bevelSegments: 2, steps: 2, bevelSize: 1, bevelThickness: 1 };
 
 		const geometry = new THREE.ExtrudeGeometry( heartShape, extrudeSettings );
 
 		const mesh = new THREE.Mesh( geometry, new THREE.MeshBasicMaterial({color:"red"}) );
-		scene.add(mesh)
+		scene?.add(mesh)
+		console.log(`scene:`,scene)
 		// mesh.scale.x=1/ratio
 		// mesh.scale.y=1/ratio
 		// mesh.scale.z=1/ratio
+		// mesh.position.set(x,0,-y)
 
 	})
-		return ()=>{
-			destroyList.forEach(fun=>fun())
-		}
-	}, []);
 	function onChange(list){
-		console.log(`list:`,list)
-		
+		setList(list)
 	}
+	const [renderState, setRenderState] = useState("画线");
 	return (
 		<>
+			<div className="control" style={{
+				position: "fixed",
+				top: 0,
+				left: 0,
+				zIndex: 2,
+				display: "flex",
+				flexDirection: "column",
+				alignItems: "center",
+				justifyContent: "center",
+			}}>
+				<button onClick={()=>setRenderState("画线")}>画线</button>
+				<button onClick={()=>setRenderState("渲染")}>渲染</button>
+			</div>
 			<canvas
 				style={{ display: "block", width: "100vw", height: "100vh" }}
 				ref={canvasRef}
 			/>
 			<div
 				style={{
-					display: "block",
+					// display: "none",
+					// display: "block",
+					display:renderState==="画线"?"block":"none",
 					width: "100vw",
 					height: "100vh",
 					position: "absolute",
